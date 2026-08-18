e706f77c60d49568671fcef962379855d364e54d
Leon Fan <526247082@qq.com>
2026-08-18 19:12:41 +0800

    fix: center Quick Picks cards on home page
    Change grid from 4 columns to 3 columns so the three Most In-Depth Guides cards fill the row and stay centered on wide screens

--- src/components/home/quick-picks.tsx
+++ src/components/home/quick-picks.tsx
@@ -65,7 +65,7 @@ export function QuickPicks() {
       </div>
 
       {/* Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {quickPicks.map((pick) => (
           <Link
             key={pick.slug}
